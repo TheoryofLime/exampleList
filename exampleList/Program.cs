c7058c8419b5765291cb0f654c0775677cc1376a
user <user@PC>
2023-03-12 20:44:19 -0400

    Add project files.

---
+++ exampleList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace exampleList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> newList = new List<string>();
+            // add
+            newList.Add("A");
+            newList.Add("B");
+            newList.Add("C");
+            newList.Add("D");
+            newList.Add("E");
+            // add range 
+            string[] extraData = new string[] { "F", "G", "H" };
+            newList.AddRange(extraData);
+            // sort
+            newList.Sort();
+            // remove
+            newList.Remove("A");
+            // reverse sort and display
+            newList.Reverse();
+            foreach (string item in newList) Console.WriteLine(item);
+        }
+    }
+}
